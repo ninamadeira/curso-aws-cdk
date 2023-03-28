5093bc652fb1efe7f71401b0047bc602c3e48498
Nina Madeira <nina.madeira@gmail.com>
2023-03-28 00:25:22 -0300

    Subindo projeto do curso cdk aws

---
+++ src/main/java/com/myorg/Service04Stack.java
@@ -0,0 +1,84 @@
+package com.myorg;
+
+import software.amazon.awscdk.Duration;
+import software.amazon.awscdk.Fn;
+import software.amazon.awscdk.RemovalPolicy;
+import software.amazon.awscdk.Stack;
+import software.amazon.awscdk.StackProps;
+import software.amazon.awscdk.services.applicationautoscaling.EnableScalingProps;
+import software.amazon.awscdk.services.ecs.AwsLogDriverProps;
+import software.amazon.awscdk.services.ecs.Cluster;
+import software.amazon.awscdk.services.ecs.ContainerImage;
+import software.amazon.awscdk.services.ecs.CpuUtilizationScalingProps;
+import software.amazon.awscdk.services.ecs.LogDriver;
+import software.amazon.awscdk.services.ecs.ScalableTaskCount;
+import software.amazon.awscdk.services.ecs.patterns.ApplicationLoadBalancedFargateService;
+import software.amazon.awscdk.services.ecs.patterns.ApplicationLoadBalancedTaskImageOptions;
+import software.amazon.awscdk.services.elasticloadbalancingv2.HealthCheck;
+import software.amazon.awscdk.services.events.targets.SnsTopic;
+import software.amazon.awscdk.services.logs.LogGroup;
+import software.constructs.Construct;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Service04Stack extends Stack {
+    public Service04Stack(final Construct scope, final String id, Cluster cluster, SnsTopic productEventsTopic) {
+        this(scope, id, null, cluster, productEventsTopic);
+    }
+
+    public Service04Stack(final Construct scope, final String id, final StackProps props, Cluster cluster, SnsTopic productEventsTopic) {
+        super(scope, id, props);
+        Map<String, String> envVariables = new HashMap<>();
+        envVariables.put("SPRING_DATASOURCE_URL", "jdbc:mariadb://" + Fn.importValue("rds-endpoint")
+                + ":3306/aws_project01?createDatabaseIfNotExist=true");
+        envVariables.put("SPRING_DATASOURCE_USERNAME", "admin");
+        envVariables.put("SPRING_DATASOURCE_PASSWORD", Fn.importValue("rds-password"));
+        envVariables.put("AWS_REGION", "us-west-2");
+        envVariables.put("AWS_SNS_TOPIC_PRODUCT_EVENTS_ARN", productEventsTopic.getTopic().getTopicArn());
+
+        ApplicationLoadBalancedFargateService service04 = ApplicationLoadBalancedFargateService.Builder.create(this, "ALB04")
+                .serviceName("service-04")
+                .cluster(cluster)
+                .cpu(512)
+                .memoryLimitMiB(1024)
+                .desiredCount(2)
+                .listenerPort(8080)
+                .taskImageOptions(
+                        ApplicationLoadBalancedTaskImageOptions.builder()
+                                .containerName("aws_project01")
+                                .image(ContainerImage.fromRegistry("ninamadeira/curso_aws_project01:1.1.5"))
+                                .containerPort(8080)
+                                .logDriver(LogDriver.awsLogs(AwsLogDriverProps.builder()
+                                        .logGroup(LogGroup.Builder.create(this, "Service04LogGroup")
+                                                .logGroupName("Service04")
+                                                .removalPolicy(RemovalPolicy.DESTROY)
+                                                .build())
+                                        .streamPrefix("Service04")
+                                        .build()))
+                                .environment(envVariables)
+                                .build())
+                .publicLoadBalancer(true)
+                .build();
+
+        service04.getTargetGroup().configureHealthCheck(new HealthCheck.Builder()
+                .path("/actuator/health")
+                .port("8080")
+                .healthyHttpCodes("200")
+                .build());
+
+        ScalableTaskCount scalableTaskCount = service04.getService().autoScaleTaskCount(EnableScalingProps.builder()
+                .minCapacity(2)
+                .maxCapacity(4)
+                .build());
+
+        scalableTaskCount.scaleOnCpuUtilization("Service04AutoScaling", CpuUtilizationScalingProps.builder()
+                .targetUtilizationPercent(50)
+                .scaleInCooldown(Duration.seconds(60))
+                .scaleOutCooldown(Duration.seconds(60))
+                .build());
+
+
+        productEventsTopic.getTopic().grantPublish(service04.getTaskDefinition().getTaskRole());
+
+    }
+}
